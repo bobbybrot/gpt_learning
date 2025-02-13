e14ddb358febfc03841d34cf415caff13a8872b9
Balwinder Singh <balwinder.s@flintec.com>
2025-02-13 09:20:05 +0000

    Commit of outstanding targeted exercises, given by CHATGPT.

--- data_structures/array_search_algorithms.c
+++ data_structures/array_search_algorithms.c
@@ -10,3 +10,12 @@ Input: {3, 4, -1, 1}
 Output: 2
 You should aim for a time complexity of O(n).
 */
+
+/*
+5. Two-Sum Search:
+Given a sorted array, find two distinct indices such that their elements sum up to a given target. Return the pair of indices.
+
+Example:
+Input: Array = {2, 7, 11, 15}, Target = 9.
+Output: [0, 1].
+*/
