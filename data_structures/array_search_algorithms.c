b309bd949050a55d769346e1d3a70b2f464f71f9
Balwinder Singh <balwinder.s@flintec.com>
2025-02-13 07:50:01 +0000

    First setup of project

---
+++ data_structures/array_search_algorithms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+4. First Missing Positive Integer:
+Write a function to find the smallest positive integer missing from an unsorted array.
+
+For example:
+Input: {3, 4, -1, 1}
+Output: 2
+You should aim for a time complexity of O(n).
+*/
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "data_structures/array_search_algorithms.c"
+
+int main(void)
+{
+    int arr [] = {0,1,2};
+    int size = sizeof(arr);
+
+    //ADD CODE HERE TO UNIT TEST CREATED CODE
+    return 0;
+}
