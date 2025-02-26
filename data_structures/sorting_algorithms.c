f03e6321540390e4d496f904f14e0c520996401a
Balwinder Singh <balwinder.s@flintec.com>
2025-02-26 08:26:06 +0000

    Added new sorting algorithm file to hold exercises

---
+++ data_structures/sorting_algorithms.c
@@ -0,0 +1,53 @@
+/*
+Problem:
+Given the array {45, 12, 89, 33, 77, 2, 18}, implement Quick Sort and show the array after the first partition step when choosing the last element as the pivot.
+
+Steps:
+1. Choose the pivot.
+2. Rearrange the array such that elements smaller than the pivot are on the left and larger ones on the right.
+3. Return the partitioned array after this step.
+
+Expected Output Format:
+    1.Chosen pivot: X
+    2.Array after partitioning: [ ..., ..., ..., pivot, ..., ...]
+✅ Use Quick Sort for this exercise.
+*/
+int QuickSortPartition(int arr[], int low, int high)
+{
+    
+}
+
+void QuickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex = QuickSortPartition(arr, low, high);
+        QuickSort(arr, low, pivotIndex - 1);
+        Quicksort(arr, pivotIndex + 1, high);
+    }
+}
+
+
+/*
+Problem:
+Given two sorted halves of an array:
+
+leftHalf = {4, 10, 23}
+rightHalf = {8, 15, 30}
+Manually merge these two halves into a single sorted array using Merge Sort's merging step.
+
+Expected Output: A merged array {4, 8, 10, 15, 23, 30}
+
+✅ Use Merge Sort for this exercise.
+*/
+
+
+/*
+Problem:
+Given the nearly sorted array {2, 5, 8, 10, 7, 15, 20}, apply Insertion Sort only for the misplaced number(s) to restore the sorted order.
+
+Expected Output: Show array before and after fixing the order.
+
+Show the array before and after fixing the order.
+✅ Use Insertion Sort for this exercise.
+*/
--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include "data_structures/array_search_algorithms.c"
+#include "data_structures/sorting_algorithms.c"
 
 int main(void)
 {
-    int arr[] = {1, 3, 5, 7, 9};
-    int target = 8;
-    indices data = TwoSumSearch(arr, (sizeof(arr)/4), target);
-
-    printf("[%d, %d]", data.indiceOne, data.indicesTwo);
+    //Add code here!
     return 0;
 }
